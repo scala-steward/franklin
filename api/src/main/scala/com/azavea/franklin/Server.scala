ebf652a8c563955845a363e206fb1cee8ab5cac7
Chris Brown <cbrown@azavea.com>
2019-11-08 19:57:39 -0500

    WIP

--- api/src/main/scala/com/azavea/franklin/Server.scala
+++ api/src/main/scala/com/azavea/franklin/Server.scala
@@ -36,16 +36,6 @@ import java.util.concurrent.Executors
 
 object Server extends IOApp {
 
-  val rasterIO: ContextShift[IO] = IO.contextShift(
-    ExecutionContext.fromExecutor(
-      Executors.newCachedThreadPool(
-        new ThreadFactoryBuilder().setNameFormat("franklin-api-%d").build()
-      )
-    )
-  )
-
-  override implicit val contextShift: ContextShift[IO] = rasterIO
-
   val banner =
     """
 
@@ -63,19 +53,25 @@ $$$$
 
 """.split("\n").toList
 
-  def createServer(port: Int): Resource[IO, HTTP4sServer[IO]] =
+  def createServer(
+      port: Int,
+      dbUser: String,
+      dbPass: String,
+      dbHost: String,
+      dbPort: Int,
+      dbName: String
+  ): Resource[IO, HTTP4sServer[IO]] =
     for {
       connectionEc  <- ExecutionContexts.fixedThreadPool[IO](2)
       transactionEc <- ExecutionContexts.cachedThreadPool[IO]
-
-      xa <- {
-        HikariTransactor
-          .fromHikariConfig[IO](
-            DatabaseConfig.hikariConfig,
-            connectionEc,
-            transactionEc
-          )
-      }
+      xa <- HikariTransactor.newHikariTransactor(
+        "org.postgresql.Driver",
+        s"jdbc:postgresql://$dbHost:$dbPort/$dbName",
+        dbUser,
+        dbPass,
+        connectionEc,
+        transactionEc
+      )
       allEndpoints      = LandingPageEndpoints.endpoints ++ CollectionEndpoints.endpoints ++ CollectionItemEndpoints.endpoints ++ SearchEndpoints.endpoints
       docs              = allEndpoints.toOpenAPI("Franklin", "0.0.1")
       docRoutes         = new SwaggerHttp4s(docs.toYaml, "open-api", "spec.yaml").routes[IO]
@@ -134,14 +130,42 @@ $$$$
     ExitCode.Success
   }
 
-  case class RunServer(port: Int)
+  final case class RunServer(
+      port: Int,
+      dbUser: String,
+      dbPass: String,
+      dbHost: String,
+      dbPort: Int,
+      dbName: String
+  )
+
+  private val databasePort = Opts
+    .option[Int]("db-port", help = "Port to connect to database on")
+    .withDefault(5432)
+
+  private val databaseHost = Opts
+    .option[Int]("db-host", help = "Database host to connect to")
+    .withDefault("database.service.internal")
+
+  private val databaseName = Opts
+    .option[String]("db-name", help = "Database name to connect to")
+    .withDefault("franklin")
+
+  private val databasePassword = Opts
+    .option[String]("db-password", help = "Database password to use")
+    .withDefault("franklin")
+
+  private val databaseUser = Opts
+    .option[String]("db-user", help = "User to connect with database with")
+    .withDefault("franklin")
+
+  private val serverPort = Opts
+    .option[Int]("port", help = "Port to start web service on")
+    .withDefault(9090)
 
   val runServerOpts: Opts[RunServer] =
     Opts.subcommand("server", "Runs web service") {
-      Opts
-        .option[Int]("port", help = "Port to start web service on")
-        .withDefault(9090)
-        .map(RunServer)
+      (serverPort, databaseUser, databasePassword, databaseHost, databasePort, databaseName) mapN (RunServer)
     }
 
   val applicationCommand: Command[Product] =
@@ -151,7 +175,10 @@ $$$$
 
   override def run(args: List[String]): IO[ExitCode] = {
     applicationCommand.parse(args) map {
-      case RunServer(port) => createServer(port).use(_ => IO.never).as(ExitCode.Success)
+      case RunServer(port, dbUser, dbPassword, dbHost, dbPort, dbName) =>
+        createServer(port, dbUser, dbPassword, dbHost, dbPort, dbName)
+          .use(_ => IO.never)
+          .as(ExitCode.Success)
       case RunMigrations() => runMigrations
       case RunImport(catalogRoot) =>
         runImport(catalogRoot).compile.drain map { _ =>
